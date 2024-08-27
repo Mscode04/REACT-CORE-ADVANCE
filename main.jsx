7fa1484938eaadd8de1a44d4f52fe32112444e17
Mscode04 <ms.shaheenkp@gmail.com>
2024-08-27 15:45:42 +0530

    main

---
+++ main.jsx
@@ -0,0 +1,4 @@
+// React is a popular JavaScript library used for building user interfaces, 
+// particularly for single-page applications where you want a fast, 
+// responsive experience. Developed and maintained by Facebook, 
+// React allows developers to create large web applications that can update and render efficiently with changing data.
